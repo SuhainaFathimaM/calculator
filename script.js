a4d5dd3564d29090ac1210d73d91d96d80cc2ecf
SuhainaFathimaM <153417476+SuhainaFathimaM@users.noreply.github.com>
2023-12-21 09:33:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const op = prompt('Enter operator ( +,-,*,/): ');   
-const num1 = parseFloat(prompt ('Enter the first number: '));  
-const num2 = parseFloat(prompt ('Enter the second number: '));  
-let res; 
-if (op == '+') {  
-    res = num1 + num2;  
-}  
-else if (op == '-') {  
-    res = num1 - num2;  
-}  
-else if (op == '*') { 
-    res = num1 * num2;  
-}  
-else {  
-    res = num1 / num2;
-}  
-  
-window.alert(" Result is " + res+ " \n Thank You!!!");  
+
+
+
+function appendValue(value) {
+    document.form1.answer.value += value;
+}
+
+function calculate() {
+    document.form1.answer.value = eval(document.form1.answer.value);
+}
+
+function clearAll() {
+    document.form1.answer.value = '';
+}
+
+
+
+
+
+
+
+

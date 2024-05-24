e9198ad15ac37dbe96d62b6f586413aa71aeb74f
SuhainaFathimaM <suhainafathimam@gmail.com>
2024-05-24 18:08:53 +0530

    updated light and dark mode

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 function appendValue(value) {
     document.form1.answer.value += value;
 }
@@ -13,9 +11,40 @@ function clearAll() {
     document.form1.answer.value = '';
 }
 
+const modeswitch=document.getElementById("mode-switch");
+const body=document.body;
+modeswitch.addEventListener("change",()=>
+{
+    if(modeswitch.checked)
+        {
+            body.classList.add("dark-mode");
+        }
+        else{
+            body.classList.remove("dark-mode");
+        }
+});
 
+const micBtn = document.getElementById('mic-btn');
+const recognition = new webkitSpeechRecognition(); // Use webkitSpeechRecognition for Chrome
 
+micBtn.addEventListener('click', () => {
+    recognition.start();
+});
 
+recognition.onresult = (event) => {
+    const transcript = event.results[0][0].transcript;
+    document.form1.answer.value = transcript;
+    calculate(); // Calculate after voice input
+};
+
+recognition.onerror = (event) => {
+    console.error("Speech recognition error:", event);
+};
+
+// Calculate after voice input
+recognition.onend = () => {
+    calculate(); 
+};
 
 
 
